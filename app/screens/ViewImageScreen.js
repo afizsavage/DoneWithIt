06f2e6cd663405f3ac16e221181b00892156c648
Afiz Savage <savageafiz9@gmail.com>
2021-04-13 15:04:11 +0000

    app init

---
+++ app/screens/ViewImageScreen.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+
+function ViewImageScreen(props) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.closeIcon}></View>
+      <View style={styles.deleteIcon}></View>
+      <Image
+        resizeMode="contain"
+        style={styles.image}
+        source={require("../assets/chair.jpg")}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#fc5c65",
+    position: "absolute",
+    top: 40,
+    left: 30,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#000",
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#4ecdc4",
+    position: "absolute",
+    top: 40,
+    right: 30,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default ViewImageScreen;
