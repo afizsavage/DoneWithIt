926c178ebef66ccbf7622982e3010420dcb0b5e6
Chance <savageafiz9@gmail.com>
2021-05-11 20:26:34 +0000

    merge conflict resolved

--- App.js
+++ App.js
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import * as ImagePicker from "expo-image-picker";
-import * as Permissions from "expo-permissions            ";
-
-import Screen from "./app/components/Screen";
-import AppButton from "./app/components/AppButton";
-import { Image } from "react-native";
-
-export default function App() {
-  const [ImageUri, setImageUri] = useState(initialState);
-
-  const requestPermision = async () => {
-    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (!granted) alert("You need to enable permission to access the library");
-  };
-
-  useEffect(() => {
-    requestPermision();
-  }, []);
-
-  const selectImage = async (params) => {
-    try {
-      const result = await ImagePicker.launchImageLibraryAsync();
-      if (!result.cancelled) {
-        setImageUri(result.uri);
-      }
-    } catch (error) {
-      console.log("Error reading an image");
-    }
-  };
-
-  return (
-    <Screen>
-      <AppButton title="Select Image" onPress={selectImage} />
-      <Image source={{ uri: ImageUri }} style={{ width: 200, height: 200 }} />
-    </Screen>
-  );
-=======
 import React, { useState } from "react";
 import LoginScreen from "./app/screens/LoginScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
@@ -44,5 +5,4 @@ import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
   return <ListingEditScreen />;
->>>>>>> 57cac9d616e6be9e0109d24b050f29a6671dac53
 }
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import { Image, StyleSheet } from "react-native";
-
-import AppButton from "../components/AppButton";
-import AppTextInput from "../components/AppTextInput";
-import Screen from "../components/Screen";
-
-function LoginScreen(props) {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  return (
-    <Screen style={styles.container}>
-      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-      <AppTextInput
-        autoCapitalize="none"
-        autoCorrect={false}
-        icon="email"
-        onChangeText={(text) => setEmail(text)}
-        keyboardType="email-address"
-        placeholder="Email"
-        textContentType="emailAddress"
-      />
-      <AppTextInput
-        autoCapitalize="none"
-        autoCorrect={false}
-        icon="lock"
-        onChangeText={(text) => setPassword(text)}
-        placeholder="Password"
-        secureTextEntry
-        textContentType="password"
-      />
-      <AppButton title="Login" onPress={() => console.log(email, password)} />
-=======
 import React from "react";
 import { Image, StyleSheet } from "react-native";
 import * as Yup from "yup";
@@ -44,7 +10,7 @@ const validationSchema = Yup.object().shape({
   password: Yup.string().required().min(4).label("Password"),
 });
 
-function LoginScreen(props) {
+function LoginScreen() {
   return (
     <Screen style={styles.container}>
       <Image style={styles.logo} source={require("../assets/logo-red.png")} />
@@ -74,7 +40,6 @@ function LoginScreen(props) {
         />
         <SubmitButton title="Login" />
       </AppForm>
->>>>>>> 57cac9d616e6be9e0109d24b050f29a6671dac53
     </Screen>
   );
 }
